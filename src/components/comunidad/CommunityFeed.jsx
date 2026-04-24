7f8e100bd3ffc4596e3e1d4de00fcfee306b8044
Valeria Cabrera <valeriacabrera45x@gmail.com>
2026-04-24 04:56:50 +0200

    v146

--- src/components/comunidad/CommunityFeed.jsx
+++ src/components/comunidad/CommunityFeed.jsx
@@ -87,21 +87,11 @@ export default function CommunityFeed({
               style={{
                 borderRight: index % 2 === 0 ? COMMUNITY_BORDERS.soft : "none",
                 borderBottom: COMMUNITY_BORDERS.light,
-                minHeight: 150,
               }}
             >
               <PostCard post={post} query={query} showBottomBorder={false} />
             </div>
-          ) : (
-            <div
-              key={`empty-${index}`}
-              style={{
-                borderRight: index % 2 === 0 ? COMMUNITY_BORDERS.soft : "none",
-                borderBottom: COMMUNITY_BORDERS.light,
-                minHeight: 150,
-              }}
-            />
-          )
+          ) : null
         )}
       </Grid>
     </>
